d859206a58850835bd0b9e77e180aea5273c6453
27Tobiik <134804311+27Tobiik@users.noreply.github.com>
2026-02-09 19:51:32 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+
+Config.Language = {
+    enter_trunk = "Press [E] to exit the trunk",
+    already_in_trunk = "Someone is already in the trunk!",
+    vehicle_locked = "Vehicle is locked!",
+    no_vehicle_nearby = "No vehicle nearby!",
+    me_enter_trunk = "climbs into the trunk",
+    me_exit_trunk = "climbs out of the trunk",
+    hide_trunk_label = "Hide in Trunk / Exit"
+}
+
---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '27Tobiik'
+description 'Schování do kufru'
+version '1.0.0'
+
+client_script 'client/client.lua'
+
+shared_scripts {
+	'@ox_lib/init.lua',
+}
+
+dependencies {
+    'ox_target'
+    'ox_lib'
+}
